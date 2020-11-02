854ccde5ec0c0b024e0ed12eeb778392b5d13437
SoHyun <pje1740@gmail.com>
2020-11-02 22:33:12 +0900

    [FEAT] add cors
    [why]
    To resolve cors errors.
    
    [what]
    app.js
    
    [etc]

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
 const router = require('./Routes/index');
+const cors = require('cors');
 
 const app = express();
 app.use(logger('short'));
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/v1', router);
 
