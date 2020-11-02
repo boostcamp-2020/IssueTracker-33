de22acc64ca4b8431957d161bfcd6e1ee2001624
SoHyun <pje1740@gmail.com>
2020-11-02 18:58:29 +0900

    [FEAT] create DB connection using mysql2
    [why]
    To connect DB.
    
    [what]
    dbPool.js
    
    [etc]

---
+++ Backend/Models/dbPool.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+module.exports = { db };
