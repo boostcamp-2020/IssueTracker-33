0b996d53a985d0a10717f07d6fa9a09a4ab42375
JeesooHaa <haajeesoo@gmail.com>
2020-11-02 16:12:51 +0900

    [CHORE] add front template
    [why]
    for start frontend
    
    [what]
    add App.js
    update index.js and index.html
    remove basic.js

--- Frontend/Views/index.js
+++ Frontend/Views/index.js
@@ -1,17 +1,7 @@
-import Basic from './basic'
-import React, {Fragment} from 'react';
-import ReactDOM, {render}from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
+const root = document.querySelector('#root');
 
-render(
-    <Fragment>
-        <h1>Hello, world!</h1>
-        <h1 className="isit">이건 되냔말이야</h1>
-        <Basic />
-    </Fragment>,
-    document.getElementById('root')
-)
-
-console.log(process.env.NODE_ENV)
-alert('aaa')
-console.log('asdad')
+ReactDOM.render(<App />, root);
