d7df5c3340aac3b47abd481b7c0bdb52e45afef0
SoHyun <pje1740@gmail.com>
2020-11-03 15:09:43 +0900

    [FEAT] add checking logic for empty title and comment in the form.
    [why]
    We need to prevent the users from leaving the title and comment section empty.
    
    [what]
    NewIssueForm.js
    
    [etc]

--- Frontend/Views/Components/NewIssueForm.js
+++ Frontend/Views/Components/NewIssueForm.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
+const ErrorMessage = ({ message }) => {
+  return (
+    <>
+      <div>{message}</div>
+    </>
+  );
+};
 
 const NewIssueForm = () => {
   const [title, setTitle] = useState('');
   const [comment, setComment] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [commentError, setCommentError] = useState(false);
+  const [errorExisting, setErrorExisting] = useState(false);
+
+  useEffect(() => {
+    setErrorExisting(titleError || commentError);
+  }, [titleError, commentError]);
 
   // api 모듈로 분리
   const postIssue = async () => {
@@ -27,6 +42,16 @@ const NewIssueForm = () => {
   };
 
   const onSubmit = (e) => {
+    // title, comment 을 받아서 trim 후 스페이스 뿐이라면 에러 메시지 출력
+    if (title.trim() === '') {
+      setTitleError(true);
+    }
+    if (comment.trim() === '') {
+      setCommentError(true);
+    }
+    if (setErrorExisting) {
+      return;
+    }
     e.preventDefault();
     createIssue();
   };
@@ -42,6 +67,7 @@ const NewIssueForm = () => {
   return (
     <div>
       <input type="text" placeholder="Title" onChange={onChangeTitle} />
+      {titleError && <ErrorMessage message="제목을 입력해주세요." />}
       <input
         type="text"
         placeholder="Leave a comment"
@@ -52,6 +78,7 @@ const NewIssueForm = () => {
         type="file"
         accept="image/png, image/jpeg"
       />
+      {commentError && <ErrorMessage message="본문을 입력해주세요." />}
       <button type="button">Cancel</button>
       <button type="submit" onClick={onSubmit}>
         Submit new issue
