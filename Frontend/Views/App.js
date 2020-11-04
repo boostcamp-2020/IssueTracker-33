0bd9accbaa080eb0f7322a3abc8a0f2deae199c0
marulloc <marullo@naver.com>
2020-11-04 15:19:05 +0900

    [FEAT] React Route basic set
    [why]
    for using url routing with react-router
    
    [what]
    install react-router-dom
    and use <BrowserRouter>as<Router>, <Route> tag
    
    finally
    can access "main page" with url "/"
    can access "new issue page" with url"/new"
    
    [etc]
    etc...

--- Frontend/Views/App.js
+++ Frontend/Views/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import NewIssuePage from './Pages/NewIssuePage';
+import IssueMainPage from './Pages/IssueMainPage';
 
 const App = () => {
   return (
-    <>
-      <NewIssuePage />
-    </>
+    <Router>
+      <Route exact path="/" component={IssueMainPage} />
+      <Route path="/new" component={NewIssuePage} />
+    </Router>
   );
 };
 
---
+++ Frontend/Views/Pages/IssueMainPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssueMainPage = () => {
+  return (
+    <div>
+      <h1>issueMain</h1>
+    </div>
+  );
+};
+
+export default IssueMainPage;
