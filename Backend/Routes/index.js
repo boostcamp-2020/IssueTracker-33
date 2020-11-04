ce4a7efc3e5e1b6f40667f0ad292ac1e450376a9
SoHyun <pje1740@gmail.com>
2020-11-04 15:06:12 +0900

    [FEAT] Create a route for uploading image on the image server.
    [why]
    We need this api to upload image and retreive the image url.
    
    [what]
    index.js, imageRoute.js
    
    [etc]

--- Backend/Routes/index.js
+++ Backend/Routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const issueRoute = require('./issueRoute');
+const imageRoute = require('./imageRoute');
 
 router.use('/issues', issueRoute);
+router.use('/images', imageRoute);
 
 module.exports = router;
