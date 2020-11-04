c4ffefc194c141a22823cbbd259f9e1ab31be7f3
JeesooHaa <haajeesoo@gmail.com>
2020-11-04 15:35:05 +0900

    [FIX] update selectedData props
    [why]
    To receive data from parent component
    
    [what]
    add state and function

--- Frontend/Views/Components/NewIssueDropdown.js
+++ Frontend/Views/Components/NewIssueDropdown.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import NewIssueList from './NewIssueList';
 
-const NewIssueDropdown = ({ dropdownTitle, children }) => {
+const NewIssueDropdown = ({ dropdownTitle, selected, setSelected, children }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selected, setSelected] = useState([]);
 
   const onToggleList = () => (isOpen ? setIsOpen(false) : setIsOpen(true));
 
