ef61103dd5db819ed9dccda87f75742b04e7e678
Dokyun Kim <cs.dkkim@gmail.com>
2020-11-04 17:50:50 +0900

    [FEAT] add issue list page boilerplate
    [why]
    To collaborate with pje1740
    
    [what]
    Update 'App.js'
    Add 'IssuesPage.js'
    Add 'IssueList.js'
    
    [etc]

--- Frontend/Views/App.js
+++ Frontend/Views/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import NewIssuePage from './Pages/NewIssuePage';
+import IssuesPage from './Pages/IssuesPage';
 
 const App = () => {
   return (
     <>
+      {/* TODO: add URL router */}
+      <IssuesPage />
       <NewIssuePage />
     </>
   );
---
+++ Frontend/Views/Components/IssueList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// TODO: 헤더(전체 체크박스 + ALMA) 컴포넌트 정의
+
+// TODO: 별개의 컴포넌트로 분리할 지 고민
+const issueListItem = ({}) => {
+  return <div>Items</div>;
+};
+
+const IssueList = () => {
+  return (
+    <div>
+      <h1>Hi! I&apos;m IssueList Component</h1>
+    </div>
+  );
+};
+
+export default IssueList;
