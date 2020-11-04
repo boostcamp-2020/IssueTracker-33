5103f3cecbb2ef33d9c8928cedafc41f14d8111e
Dokyun Kim <cs.dkkim@gmail.com>
2020-11-04 20:15:20 +0900

    [FEAT] implement prototype of issue list page
    [why]
    To collaborate with with marulloc
    
    [what]
    issue list item
    
    [etc]

--- Frontend/Views/Pages/IssuesPage.js
+++ Frontend/Views/Pages/IssuesPage.js
@@ -1,13 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import IssueList from '../Components/IssueList';
 
 const IssuesPage = () => {
-  useEffect(() => {}, []);
+  const [issueData, setIssue] = useState([]);
+  const [userData, setUser] = useState([]);
+  const [labelData, setLabel] = useState([]);
+  const [mileData, setMile] = useState([]);
+
+  useEffect(async () => {
+    // TODO: Append query string for issue filtering
+    const ISSUE_URL = 'http://localhost:3000/api/v1/issues';
+    const USER_URL = 'http://localhost:3000/api/v1/users';
+    const LABEL_URL = 'http://localhost:3000/api/v1/labels';
+    const MILE_URL = 'http://localhost:3000/api/v1/milestones';
+
+    const issueProm = axios.get(ISSUE_URL);
+    const userProm = axios.get(USER_URL);
+    const labelProm = axios.get(LABEL_URL);
+    const mileProm = axios.get(MILE_URL);
+
+    const [{ data: issues }, { data: users }, { data: labels }, { data: milestones }] = await Promise.all([
+      issueProm,
+      userProm,
+      labelProm,
+      mileProm,
+    ]);
+
+    setIssue(issues);
+    setUser(users);
+    setLabel(labels);
+    setMile(milestones);
+  }, []);
 
   return (
     <div>
-      <IssueList />
+      <IssueList issues={issueData} users={userData} labels={labelData} milestones={mileData} />
     </div>
   );
 };
