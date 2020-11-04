f03285af760a8d4deab794da778e5b8db9c27edb
JeesooHaa <haajeesoo@gmail.com>
2020-11-04 15:31:44 +0900

    [FEAT] add NewIssueWrapper
    [why]
    To share selected data
    
    [what]
    add NewIssueWrapper in Components

---
+++ Frontend/Views/Components/NewIssueWrapper.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import NewIssueForm from './NewIssueForm';
+import NewIssueOption from './NewIssueOption';
+
+const NewIssueWrapper = () => {
+  const [userSelectedData, setUserSelectedData] = useState([]);
+  const [labelSelectedData, setLabelSelectedData] = useState([]);
+  const [mileSelectedData, setMileSelectedData] = useState([]);
+
+  return (
+    <>
+      <NewIssueForm
+        userSelectedData={userSelectedData}
+        labelSelectedData={labelSelectedData}
+        mileSelectedData={mileSelectedData}
+      />
+      <NewIssueOption
+        userSelectedData={userSelectedData}
+        labelSelectedData={labelSelectedData}
+        mileSelectedData={mileSelectedData}
+        setUserSelectedData={setUserSelectedData}
+        setLabelSelectedData={setLabelSelectedData}
+        setMileSelectedData={setMileSelectedData}
+      />
+    </>
+  );
+};
+
+export default NewIssueWrapper;
