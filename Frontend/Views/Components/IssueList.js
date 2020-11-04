ed59b2baa3fbc88a0452a2f6265a213a51a58b84
Dokyun Kim <cs.dkkim@gmail.com>
2020-11-04 20:15:20 +0900

    [FEAT] implement prototype of issue list page
    [why]
    To collaborate with with marulloc
    
    [what]
    issue list item
    
    [etc]

--- Frontend/Views/Components/IssueList.js
+++ Frontend/Views/Components/IssueList.js
@@ -3,14 +3,16 @@ import React from 'react';
 // TODO: 헤더(전체 체크박스 + ALMA) 컴포넌트 정의
 
 // TODO: 별개의 컴포넌트로 분리할 지 고민
-const issueListItem = ({}) => {
-  return <div>Items</div>;
+const IssueListItem = ({ issue }) => {
+  return <div>{issue.title}</div>;
 };
 
-const IssueList = () => {
+const IssueList = ({ issues, users, labels, milestones }) => {
   return (
     <div>
-      <h1>Hi! I&apos;m IssueList Component</h1>
+      {issues.map((issue) => (
+        <IssueListItem key={issue.id} issue={issue} />
+      ))}
     </div>
   );
 };
