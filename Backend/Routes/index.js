c96e990dcf7a33cc7c47dd139d6f5aaeb6a1f01a
JeesooHaa <haajeesoo@gmail.com>
2020-11-05 18:04:46 +0900

    [FEAT] add commentRoute
    [why]
    To make API for comments
    
    [what]
    commentRoute.js

--- Backend/Routes/index.js
+++ Backend/Routes/index.js
@@ -4,11 +4,13 @@ const milestoneRouter = require('./milestoneRoute');
 const userRouter = require('./userRoute');
 const issueRoute = require('./issueRoute');
 const imageRoute = require('./imageRoute');
+const commentRoute = require('./commentRoute');
 
 router.use('/labels', labelRouter);
 router.use('/milestones', milestoneRouter);
 router.use('/users', userRouter);
 router.use('/issues', issueRoute);
 router.use('/images', imageRoute);
+router.use('/comments', commentRoute);
 
 module.exports = router;
