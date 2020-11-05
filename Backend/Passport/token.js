697f7523ac1a16bd05b0a1b604f23cab5480ce4a
marulloc <marullo@naver.com>
2020-11-06 02:32:45 +0900

    [CHORE] hide Tokenizing key
    [why]
    for hide json web token, secret key
    
    [what]
    add environment variable
    use .envfile
    
    [etc]
    etc...

--- Backend/Passport/token.js
+++ Backend/Passport/token.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 exports.tokenCheck = (req, res, next) => {
-  const cookie = req.cookies['jwt'];
   try {
-    jwt.verify(cookie, 'secretkey');
+    const cookie = req.cookies.jwt;
+    jwt.verify(cookie, process.env.TOKEN_SECRET_KEY);
     next();
   } catch (err) {
     res.status(401).json();
@@ -11,7 +11,7 @@ exports.tokenCheck = (req, res, next) => {
 };
 
 exports.tokenAllocate = (req, res) => {
-  const encodedToken = jwt.sign(req.user, 'secretkey');
+  const encodedToken = jwt.sign(req.user, process.env.TOKEN_SECRET_KEY);
   res.cookie('jwt', encodedToken);
   res.redirect('http://localhost:8000/issues');
 };
