f952fc2798ff4f250d803134325a9d261a2e50c3
marulloc <marullo@naver.com>
2020-11-05 14:56:46 +0900

    [FEAT] login page rendering
    [why]
    for setting passport(local & oauth)
    
    [what]
    add rendering page for login
    
    [etc]
    etc...

--- Frontend/Views/App.js
+++ Frontend/Views/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import NewIssuePage from './Pages/NewIssuePage';
 import IssueMainPage from './Pages/IssueMainPage';
+import LoginPage from './Pages/LoginPage';
 
 const App = () => {
   return (
     <Router>
+      <Route exact path="/" component={LoginPage} />
       <Route exact path="/issues" component={IssueMainPage} />
       <Route path="/issues/new" component={NewIssuePage} />
     </Router>
