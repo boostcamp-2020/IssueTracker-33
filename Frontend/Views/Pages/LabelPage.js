e6fb84a10dae379a9078f23a308481c44826dc3e
SoHyun <pje1740@gmail.com>
2020-11-09 15:47:31 +0900

    [FEAT] create label page
    [why]
    We need label page and router for labels page
    
    [what]
    App.js
    LabelPage.js
    
    [etc]

--- Frontend/Views/Pages/LabelPage.js
+++ Frontend/Views/Pages/LabelPage.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import LabelList from '../Components/LabelList';
+import LabelForm from '../Components/LabelForm';
 
 const LabelPage = () => {
+  const [labels, setLabels] = useState([]);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  useEffect(async () => {
+    const { data } = await axios.get('http://localhost:3000/api/v1/labels', { withCredentials: true });
+    setLabels(data);
+  });
+
+  const onToggleForm = () => {
+    setIsFormVisible(!isFormVisible);
+  };
+
   return (
-    <div>
-      <Link to="/labels">labels</Link>
-      <Link to="/milestones">milestones</Link>
-    </div>
+    <>
+      <div>
+        <button type="button">Lables</button>
+        <button type="button">Milestones</button>
+        <button type="button" onClick={onToggleForm}>
+          New Label
+        </button>
+      </div>
+      {isFormVisible && <LabelForm />}
+      <LabelList labels={labels} />
+    </>
   );
 };
 
