4e7b922d793ce10ed18178449c2d1caf6cf43e41
SoHyun <pje1740@gmail.com>
2020-11-10 15:11:29 +0900

    [FEAT] add labels list display and create labelItem component
    [why]
    Each label needs its own form toggling state, so I created a labelItem component to handle individual label related functions and states.
    
    [what]
    LabelList.js
    
    [etc]

--- Frontend/Views/Components/LabelList.js
+++ Frontend/Views/Components/LabelList.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import LabelForm from './LabelForm';
 
-const LabelList = ({ labels }) => {
+const LabelList = ({ labels, setLabels, setIsFormVisible }) => {
+  const onClickDelete = async (id) => {
+    const LABEL_URL = 'http://localhost:3000/api/v1/labels';
+    try {
+      await axios.delete(`${LABEL_URL}/${id}`, { withCredentials: true });
+      setLabels(labels.filter((label) => label.id !== id));
+    } catch (err) {
+      console.log('error');
+    }
+  };
+
+  const LabelItem = ({ label }) => {
+    const [isEdit, setIsEdit] = useState(false);
+    return (
+      <>
+        <div key={label.id}>
+          <span color={label.color}>{label.name}</span>
+          <span>{label.description}</span>
+          <button type="button" onClick={() => setIsEdit(!isEdit)}>
+            Edit
+          </button>
+          <button type="button" onClick={() => onClickDelete(label.id)}>
+            Delete
+          </button>
+          {isEdit && (
+            <LabelForm
+              isEdit
+              labels={labels}
+              labelToEdit={label}
+              setLabels={setLabels}
+              setIsFormVisible={setIsFormVisible}
+            />
+          )}
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
-      <div>{labels.length} labels</div>
+      <div>{`${labels.length} labels`}</div>
       {labels.map((label) => (
         <>
-          <div>
-            <span color={label.color}>{label.name}</span>
-            <span>{label.desc}</span>
-            <button type="button">Edit</button>
-            <button type="button">Delete</button>
-          </div>
+          <LabelItem label={label} />
         </>
       ))}
     </>
