03dd29334e65e76140b62f1e8ade3035d9e998e2
SoHyun <pje1740@gmail.com>
2020-11-11 22:39:41 +0900

    [FEAT] apply style to LoginPage
    [why]
    styling
    
    [what]
    LoginPage.js
    
    [etc]

--- Frontend/Views/Pages/LoginPage.js
+++ Frontend/Views/Pages/LoginPage.js
@@ -1,4 +1,77 @@
 import React from 'react';
+import styled from 'styled-components';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const GrayBackground = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #eeeeee;
+`;
+
+const PageWrapper = styled.div`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputWrapper = styled.div`
+  width: 400px;
+  height: 350px;
+  border: 1px solid #aaaaaa;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+const InputDiv = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  height: 35px;
+  border-radius: 5px;
+  border: 1px solid #aaaaaa;
+`;
+
+const GithubLogin = styled.button`
+  width: 300px;
+  height: 35px;
+  margin-top: 30px;
+  background-color: #888888;
+  box-shadow: none;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: none;
+  border: none;
+  text-align: center;
+`;
+
+const SubmitText = styled.div`
+  width: 62%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  color: white;
+  font-size: 18px;
+`;
 
 const LoginPage = () => {
   const githubOauth = () => {
@@ -6,23 +79,35 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <span>ID: </span>
-          <input type="text" />
-        </div>
-        <div>
-          <span>PWD: </span>
-          <input type="password" />
-        </div>
-      </div>
-      <div>
-        <button type="button" onClick={githubOauth}>
-          GitHub으로 로그인하기
-        </button>
-      </div>
-    </div>
+    <>
+      <GrayBackground>
+        <PageWrapper>
+          <LoginWrapper>
+            <h1>이슈트래커</h1>
+            <InputWrapper>
+              <div>
+                <InputDiv>
+                  <h3>아이디 </h3>
+                  <Input type="text" />
+                </InputDiv>
+                <InputDiv>
+                  <h3>비밀번호 </h3>
+                  <Input type="password" />
+                </InputDiv>
+              </div>
+              <div>
+                <GithubLogin type="button" onClick={githubOauth}>
+                  <SubmitText>
+                    <span>Sign in with Github</span>
+                    <GitHubIcon style={{ fontSize: 20 }} />
+                  </SubmitText>
+                </GithubLogin>
+              </div>
+            </InputWrapper>
+          </LoginWrapper>
+        </PageWrapper>
+      </GrayBackground>
+    </>
   );
 };
 
