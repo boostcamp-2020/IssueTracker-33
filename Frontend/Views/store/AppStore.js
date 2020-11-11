af750c67d64599f12200c0428d46239badb54d02
marulloc <marullo@naver.com>
2020-11-12 03:09:22 +0900

    [STYLE] Apply Context Label at label page
    [why]
    for using context labels data
    
    [what]
    remove state labels
    use context labels
    
    [etc]
    etc...

--- Frontend/Views/store/AppStore.js
+++ Frontend/Views/store/AppStore.js
@@ -8,10 +8,25 @@ const milestonesReducer = (milestones, { type, data }) => {
   }
 };
 
-const labelsReducer = (labels, { type, data }) => {
+const labelsReducer = (labels, { type, data, target }) => {
   switch (type) {
     case 'setInitial':
       return data;
+
+    case 'add':
+      return [...labels, data];
+
+    case 'targetUpdate':
+      return labels.map((label) => {
+        if (label.id === target) {
+          return Object.assign(label, data);
+        }
+        return label;
+      });
+
+    case 'delete':
+      return labels.filter((label) => label.id !== target);
+
     default:
   }
 };
