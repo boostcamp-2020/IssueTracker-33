467d5e2491585a4f1b6c6dad095b75879db009cd
JeesooHaa <haajeesoo@gmail.com>
2020-11-12 23:38:49 +0900

    [FEAT] update globalStyles
    [why]
    To use style global
    
    [what]
    Update globalStyles
    Move buttonStyles

--- Frontend/style/globalStyles.js
+++ Frontend/style/globalStyles.js
@@ -10,10 +10,11 @@ const GlobalStyle = createGlobalStyle`
 		--button-green: #32C654;
 		--header-black: #242A2E;
 		--list-gray: #F7F8FA;
-		--open-green: #2BBE4E;
-		--closed-red: #CA2431;
+		--open-green: #28A745;
+		--closed-red: #D73A4A;
 		--owner-blue: #F0F8FE;
 		--tab-blue: #0466D6;
+		--font-white: #FAFBFC;
 	}
 
 
@@ -28,8 +29,23 @@ const GlobalStyle = createGlobalStyle`
 	  box-sizing: border-box;
 	}
 
+	p {
+		margin: 0;
+	}
+
 	h3 {
 		margin: 5px;
+	}
+
+	input[type="file"] { 
+		position: absolute; 
+		width: 1px; 
+		height: 1px; 
+		padding: 0; 
+		margin: -1px; 
+		overflow: hidden; 
+		clip:rect(0,0,0,0); 
+		border: 0; 
 	}
 `;
 
